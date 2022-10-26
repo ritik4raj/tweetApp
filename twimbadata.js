3173bc198ef153902e4e9a6e4ab98db0044175e6
ritik <55265343+ritik4raj@users.noreply.github.com>
2022-10-26 18:46:56 +0530

    Add files via upload

---
+++ twimbadata.js
@@ -0,0 +1,59 @@
+ export   const tweetsData = [   
+    {
+        handle: `@TrollBot66756542 💎`,
+        profilePic: `image/booklogo.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
+            Guaranteed return on investment. HMU DMs open!!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Elon ✅`,
+        profilePic: `image/spacex.jpg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
+        replies: [
+                  {
+                handle: `@King12 ✅`,
+                profilePic: `image/casino.jpg`,
+                tweetText: `Yes! Sign me up! 😎🛩`,
+            },
+                  {
+                handle: `@Jack_traveller ✅`,
+                profilePic: `image/bookhotel.jpg`,
+                tweetText: `I went last year😴`,
+            },
+           
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@Ironman12`,
+        profilePic: `image/ironman.jpg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Are you a coder if you only know HTML?`,
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `image/logo.jpg`,
+                tweetText: `No. Onviosuly not. Go get a job in McDonald's.`,
+            },
+            {
+                handle: `@Redbus`,
+                profilePic: `image/redbus.png`,
+                tweetText: `You are wonderful just as you are! ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
